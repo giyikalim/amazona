ed57334cec32d4258b061a20b413039e2869d996
alimgiyik <alimgiyik@gmail.com>
2021-10-03 03:26:30 +0300

    heroku change

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,6 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona', {
 app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
 })
-app.get('/', (req, res) => {
-    res.send('Server is ready');
-});
 
 app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
@@ -37,6 +34,15 @@ const __dirname = path.resolve();
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/product/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+})
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
 
 app.use((err, req, res, next) => {
     res.status(500).send({ message: err.message });
